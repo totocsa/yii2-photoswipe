d4a85bfb3381f2bc0759ff8e200ad05d34d12e26
totocsa <totocsa@gmail.com>
2021-05-29 11:19:34 +0200

    Update Gallery.php

--- Gallery.php
+++ Gallery.php
@@ -155,7 +155,7 @@ $(document).on("click", ".gallery-image", function(e){
         bgOpacity: 0.7,
         showHideOpacity: true
     };  
-    $.merge(options, {$clientOptions});
+    Object.assign(options, {$clientOptions});
     var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, {$items}, options);
     gallery.init();    
 });
